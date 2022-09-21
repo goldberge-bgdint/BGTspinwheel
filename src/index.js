c2876c34ba5b08c0246c22518a1799ba1239d813
Taha Rasheed <taharasheed020@gmail.com>
2022-09-21 10:44:02 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Spinningmain from "./spinningmain/Spinningmain.jsx";
+import Spinningwheel from "./spinningwheel/Spinningwheel.jsx";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <BrowserRouter>
+      <Routes>
+      
+          <Route index element={<Spinningmain />} />
+          <Route path="/" element={<Spinningmain />} /> 
+          <Route path="/bgdspinner" element={<Spinningwheel />} /> 
+        
+      </Routes>
+    </BrowserRouter>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
